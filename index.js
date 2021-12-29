2096437ee0fbc7eff15a9c3448ac95099c1ac819
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2021-12-29 00:29:17 -0500

    second commit

--- index.js
+++ index.js
@@ -3,18 +3,49 @@ const express = require("express")
 const path = require("path")
 const Product = require("./models/products")
 const redditData = require("./data.json")
+// const { find } = require('./models/products')
 
-// async function connection() {
-//     mongoose.connect('mongodb://localhost:27017/farmStore')
-// }
-// connection().then(response=> console.log("it connected"))
-// .catch(err => console.log(err));
+async function connection() {
+    mongoose.connect('mongodb://localhost:27017/farmStore')
+}
+connection().then(response=> console.log("it connected"))
+.catch(err => console.log(err));
 
 
 const app = express()
 
-// app.set("views", path.join("__dirname", "views"))
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.set("view engine", "ejs")
+
+
+app.get("/", async (req, res) => {
+    const allProducts = await Product.find({})
+    let categories;
+    for (let category of allProducts) {
+        categories = category.name;
+    }
+
+    res.render('general',{categories})
+})
+app.post("/product", (req, res) => {
+    console.log(req.body)
+    // const newProduct = new Product({
+
+    // })
+})
+app.get('/product', async(req, res) => {
+    const allProducts = await Product.find({})
+    console.log(allProducts)
+    res.render("product/product", {allProducts})
+})
+
+app.get("/product/:id", async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    console.log(product, id)
+    res.render("product/findone", {id})
+})
 
 app.get("/r/:subreddit", (req, res) => {
     const {subreddit} = req.params;
